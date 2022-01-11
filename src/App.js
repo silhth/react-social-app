888ffca78f9cea002011e9bcba2f612e344c7a60
silvia.hth <silvia.hth@gmail.com>
2022-01-11 16:23:20 +0100

    aggiunta lazy coding e suspense

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useReducer } from "react";
+import { useReducer, Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import NewPost from "./pages/NewPost";
-import Messages from "./pages/Messages";
-import Friends from "./pages/Friends";
-import Login from "./pages/Login";
+import Loading from "./components/Loading";
+// import NewPost from "./pages/NewPost";
+// import Messages from "./pages/Messages";
+// import Friends from "./pages/Friends";
+// import Login from "./pages/Login";
 
-import Home from "./pages/Home";
+
+// import Home from "./pages/Home";
+
+const Home = lazy(() => import("./pages/Home"));
+const NewPost = lazy(() => import("./pages/NewPost"));
+const Messages = lazy(() => import("./pages/Messages"));
+const Friends = lazy(() => import("./pages/Friends"));
+const Login = lazy(() => import("./pages/Login"));
+
 
 const INIT_STATE = {
   name: "FeisBell",
@@ -50,11 +59,31 @@ function App() {
       </button>
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new-post" element={<NewPost />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/friends" element={<Friends />} />
-        <Route path="/login" element={<Login />} />
+      <Route path="/" element={
+          <Suspense fallback={<Loading/>}>
+            <Home />
+          </Suspense>
+        } />
+        <Route path="/new-post" element={
+          <Suspense fallback={<Loading/>}>
+            <NewPost />
+          </Suspense>
+        } />
+        <Route path="/messages" element={
+          <Suspense fallback={<Loading/>}>
+            <Messages />
+          </Suspense>
+        } />
+        <Route path="/friends" element={
+          <Suspense fallback={<Loading/>}>
+            <Friends />
+          </Suspense>
+        } />
+        <Route path="/login" element={
+          <Suspense fallback={<Loading/>}>
+            <Login />
+          </Suspense>
+        } />
       </Routes>
 
       <Footer />
